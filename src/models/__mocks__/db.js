8c660c44140d40ab179571af7396fd02bf9c9148
Edilson <eddylson.londonio@gmail.com>
2021-10-18 01:01:35 -0500

    Structure for test

---
+++ src/models/__mocks__/db.js
@@ -0,0 +1,7 @@
+
+export const Database = jest.fn();
+
+
+const mock = jest.fn().mockImplemetation( () => {
+    return { db: { }}
+})
---
+++ src/models/__mocks__/user.js
@@ -0,0 +1,5 @@
+
+
+export const mockUser = {
+    create(data){ return data } 
+}
