6bd534a6148b7670ee53a3fbbc4540677df463f0
Edilson <eddylson.londonio@gmail.com>
2021-10-19 19:53:35 -0500

    Feat: Get prices services by ids and vs_currencies into coingecko api

--- src/services/coingecko.js
+++ src/services/coingecko.js
@@ -1,5 +1,4 @@
 import CoinGecko from 'coingecko-api';
-import { async } from 'regenerator-runtime';
 
 export default function coinGeckoService() {
   const coingGeckoClient = new CoinGecko();
@@ -8,12 +7,26 @@ export default function coinGeckoService() {
     ping: async () => {
       return await coingGeckoClient.ping();
     },
-    getCoins: async ({ currency, ids, perPage }) => {
+    getCoins: async ({ currency, ids, perPage, order, page }) => {
+      const orderBy = {
+        top_asc: 'market_cap_asc',
+        top_desc: 'market_cap_desc'
+      }
       return await coingGeckoClient.coins.markets({
         vs_currency: currency,
         ids: ids  || [],
-        per_page: perPage || 100
+        per_page: perPage || 100,
+        page: page || 1,
+        order: orderBy[order] || orderBy.top_desc
+
       });
     },
+    getPrices: async ({ ids, vs_currencies}) => {
+      return await coingGeckoClient.simple.price({
+        ids,
+        vs_currencies
+      })
+
+    }
   };
 }
