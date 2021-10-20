c74be6c616b5fb926c95265b46a3982826ab30da
Edilson <eddylson.londonio@gmail.com>
2021-10-20 00:09:24 -0500

    Test: Crypto currency test

--- src/routes/cypto-currency.js
+++ src/routes/cypto-currency.js
@@ -5,8 +5,8 @@ export default function (app, db, services) {
   const cryptoCurrencyController = controllerCryptoCurrency(services, db);
   const validator = Validator(services, db);
   /** TODO: move checkAuth to validator */
-  router.get('/currencies', cryptoCurrencyController.list);
-  router.post('/currencies', cryptoCurrencyController.create);
+  router.get('/currencies',validator.list, cryptoCurrencyController.list);
+  router.post('/currencies',validator.create ,cryptoCurrencyController.create);
   router.get('/currencies/top',validator.top ,cryptoCurrencyController.top);
   return router;
 }
--- src/validators/crypto-currency.js
+++ src/validators/crypto-currency.js
@@ -6,8 +6,15 @@ export default function (services, db) {
 
     const auth = authMiddleware(services, db);
     return {
+        create:[
+            auth.checkAuth
+
+        ],
+        list: [
+            auth.checkAuth
+        ],
         top:[
-            query('top').isInt({ min: 1, max: 25})
+            query('top').isInt({ min: 0, max: 25})
             .withMessage('validators.query.top.invalidRange'),
             validate,
             auth.checkAuth
