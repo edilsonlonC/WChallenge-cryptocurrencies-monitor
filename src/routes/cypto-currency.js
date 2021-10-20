60d935642cdc5fb40e5ca658a2cea787218a0ec5
Edilson <eddylson.londonio@gmail.com>
2021-10-19 23:55:13 -0500

    Feat: top query <= 25

--- src/routes/cypto-currency.js
+++ src/routes/cypto-currency.js
@@ -1,11 +1,12 @@
 import controllerCryptoCurrency from '../controllers/crypto-currency/crypto-currency';
-
+import Validator from '../validators/crypto-currency';
 export default function (app, db, services) {
   const router = app.Router();
   const cryptoCurrencyController = controllerCryptoCurrency(services, db);
+  const validator = Validator(services, db);
   /** TODO: move checkAuth to validator */
   router.get('/currencies', cryptoCurrencyController.list);
   router.post('/currencies', cryptoCurrencyController.create);
-  router.get('/currencies/top', cryptoCurrencyController.top);
+  router.get('/currencies/top',validator.top ,cryptoCurrencyController.top);
   return router;
 }
--- src/validators/crypto-currency.js
+++ src/validators/crypto-currency.js
@@ -7,7 +7,7 @@ export default function (services, db) {
     const auth = authMiddleware(services, db);
     return {
         top:[
-            query('top').isNumeric().isLength({ min: 0, max: 25})
+            query('top').isInt({ min: 1, max: 25})
             .withMessage('validators.query.top.invalidRange'),
             validate,
             auth.checkAuth
