0dd06894f14165a5a6451b389cd03274259d12c0
Edilson <eddylson.londonio@gmail.com>
2021-10-19 19:52:13 -0500

    Feat: List of top of crypto currencies by user

--- src/controllers/crypto-currency/crypto-currency.js
+++ src/controllers/crypto-currency/crypto-currency.js
@@ -1,12 +1,17 @@
 import response from '../../helper/response';
 
 export default function (services, db) {
-  const { CryptoCurrency } = db;
+  const { CryptoCurrency, FavoriteCurrency } = db;
   return {
     async list(req, res, next) {
       try {
         const { Currency } = req.User;
-        const data = await services.coinGecko.getCoins({ currency: Currency.name});
+        const { per_page, page} = req.query
+        const data = await services.coinGecko.getCoins({ 
+          currency: Currency.name,
+          perPage: per_page,
+          page
+        });
         const cryptoCurrencies = data.data.map((d) => ({
           id: d.id,
           symbol: d.symbol,
@@ -45,6 +50,55 @@ export default function (services, db) {
       return next(error)
     }
 
+  },
+  async top(req, res, next) {
+    const { top, order } = req.query; 
+    try {
+      const { id, Currency } = req.User;
+      const cryptoCurrenciesByUser = await CryptoCurrency.findAll({
+        where:{
+          userId: id
+        }
+      }) 
+      const ids = cryptoCurrenciesByUser.map(crypto => crypto.id);
+      const data = await services.coinGecko.getCoins({
+        ids,
+        perPage: top,
+        currency: Currency.name,
+        order
+       })
+       const allCryptoCurrencies = data.data.slice(0,top)
+      const cryptoCurrencies = allCryptoCurrencies.map((d) => ({
+          id: d.id,
+          symbol: d.symbol,
+          name: d.name,
+          image: d.image,
+          current_price: d.current_price,
+          last_updated: d.last_updated,
+        }));
+        const currencies = await FavoriteCurrency.findAll();
+        const vsCurrencies = currencies.map(currency => currency.name)
+        const dataPrices = await services.coinGecko.getPrices({
+          ids,
+          vs_currencies: vsCurrencies
+        })
+        const simplePrices = dataPrices.data
+        const cryptoCurrenciesWithPrices = cryptoCurrencies.reduce((previous, current) => {
+          previous.push({ ...current, ...simplePrices[current.id]})
+          return previous;
+          
+
+        }, [])
+        return response(res,req)({
+          data: { count: cryptoCurrencies.length, CryptoCurrencies: cryptoCurrenciesWithPrices},
+          message: 'cryptoCurencies.list'
+        })
+      
+      
+    } catch (error) {
+      return next(error)
+      
+    }
   }
   };
 }
